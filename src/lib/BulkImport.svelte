plutonium-239/personal-dictionary

commit f7e406eb927a32043cef2e7af334584ee7a130fd

combine word, pos when screen is md, improve some css

src/lib/BulkImport.svelte
@@ -99,7 +99,7 @@
 			class="flex w-full justify-between items-center"
 		>
 			Import a list of words
-			<div class="inline-flex gap-2 flex-col md:flex-row justify-center outline-offset-8 outline outline-1 outline-accent/10">
+			<div class="inline-flex gap-2 flex-col md:flex-row items-center outline-offset-8 outline outline-1 outline-accent/10">
 				<label for="separator_choice" class="text-accent font-normal">
 					Separator:
 				</label>
@@ -111,10 +111,10 @@
 					class="font-mono"
 					on:change={() => parseMultiple(formatList(inputText))}
 				>
-					<ToggleOption value=",">,</ToggleOption>
-					<ToggleOption value={`\n`}>\n</ToggleOption>
-					<ToggleOption value={`\r\n`}>\r\n</ToggleOption>
-					<ToggleOption value=" ">(space)</ToggleOption>
+					<ToggleOption classes={{option: "py-2 !mt-0"}} value=",">,</ToggleOption>
+					<ToggleOption classes={{option: "py-2 !mt-0"}} value={`\n`}>\n</ToggleOption>
+					<ToggleOption classes={{option: "py-2 !mt-0"}} value={`\r\n`}>\r\n</ToggleOption>
+					<ToggleOption classes={{option: "py-2 !mt-0"}} value=" ">(space)</ToggleOption>
 				</ToggleGroup>
 			</div>
 		</div>
@@ -131,8 +131,8 @@
 			<Button variant="fill-light" color="primary">Close</Button>
 		</div>
 		<ToggleGroup variant="underline" rounded value="input">
-			<ToggleOption value="input">Input</ToggleOption>
-			<ToggleOption value="output">Output</ToggleOption>
+			<ToggleOption classes={{option: "py-1 !mt-1"}} value="input">Input</ToggleOption>
+			<ToggleOption classes={{option: "py-1 !mt-1"}} value="output">Output</ToggleOption>
 			<svelte:fragment slot="panes">
 				<TogglePanel>
 					<TextField

src/lib/table-elements/Word.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
-	export let wordString: string
+	import type { WordData } from "../data"
+	let partOfSpeech: string
+	export let word: WordData
+
+	$: if (word.fetched) {
+		partOfSpeech = word.meanings.partOfSpeech[word.selectedDefinition]
+	}
 </script>
 
 <div class="text-center font-bold text-accent-900 dark:text-accent-50">
-	<span>{wordString}</span>
+	<div>{word.word}</div>
+	<span
+		class="pos px-2 rounded font-mono font-bold italic text-primary-content bg-primary md:hidden"
+	>
+		{partOfSpeech.slice(0, 4)}
+	</span>
 </div>

src/lib/TabularView.svelte
@@ -7,7 +7,7 @@
 	import { data } from "./store"
 	import Actions from "./table-elements/Actions.svelte"
 	import { search20, loadWordList } from "./load_wordlist"
-	import { Button, debounceEvent, ListItem, sticky, TextField, ThemeInit } from "svelte-ux"
+	import { Button, ListItem, TextField } from "svelte-ux"
 	import Word from "./table-elements/Word.svelte"
 	import Synonyms from "./table-elements/Synonyms.svelte"
 	import BulkImport from "./BulkImport.svelte"
@@ -24,8 +24,8 @@
 	const columns = table.createColumns([
 		table.column({
 			header: "Word",
-			accessor: "word",
-			cell: ({ value }) => createRender(Word, { wordString: value }),
+			accessor: (item) => item,
+			cell: ({ value }) => createRender(Word, { word: value }),
 		}),
 		table.column({
 			header: "POS",
@@ -83,15 +83,16 @@
 	let hintNode: HTMLDivElement
 	let hintHeight: number = 38
 	let textField: TextField
+	let innerWidth: number = 1000
+
 	$: hintHeight = hintNode?.clientHeight
 
 	$: if (!newWord) completionSelected = false
-	
 </script>
 
-<ThemeInit />
-
-<div class="pt-8 flex justify-center items-stretch gap-2 bg-surface-200">
+<!-- <ThemeInit /> -->
+<svelte:window bind:innerWidth />
+<div class="pt-8 flex max-md:flex-wrap justify-center items-stretch gap-2 bg-surface-200">
 	<TextField
 		bind:this={textField}
 		placeholder="Enter a new word"
@@ -158,7 +159,6 @@ class="m-2 mb-6 overflow-y-auto text-center
 	{/await}
 </div>
 
-<!-- TODO: Combine Word,POS columns when small (maybe using grid layout option?) -->
 <div class="pb-6 w-full">
 	<table {...$tableAttrs} class="w-full">
 		<thead>
@@ -167,14 +167,18 @@ class="m-2 mb-6 overflow-y-auto text-center
 					<tr {...rowAttrs}>
 						{#each headerRow.cells as cell (cell.id)}
 							<Subscribe attrs={cell.attrs()} let:attrs>
-								{@const synclass =
+								{@const synclass = cell.label === "Synonyms" ? 
+									"hidden lg:table-cell" : ""}
+								{@const posclass = cell.label === "POS" ? 
+									"hidden md:table-cell" : ""}
+								<!-- {@const synclass =
 									cell.label === "Synonyms"
 										? "hidden lg:table-cell"
-										: ""}
+										: ""} -->
 								<th
 									{...attrs}
 									class={"border-y p-2 border-solid border-secondary-900 " +
-										synclass}
+										synclass + posclass}
 								>
 									<Render of={cell.render()} />
 								</th>
@@ -195,14 +199,14 @@ class="m-2 mb-6 overflow-y-auto text-center
 								props={cell.props()}
 								let:props
 							>
-								{@const synclass =
-									cell.column.header === "Synonyms"
-										? "hidden lg:table-cell"
-										: ""}
+								{@const synclass = cell.column.header === "Synonyms" ? 
+									"hidden lg:table-cell" : ""}
+								{@const posclass = cell.column.header === "POS" ? 
+									"hidden md:table-cell" : ""}
 								<td
 									{...attrs}
 									class={"border-b p-2 border-solid border-secondary-900 " +
-										synclass}
+										synclass + posclass}
 									class:matches={props.tableFilter.matches}
 								>
 									<Render of={cell.render()} />

src/lib/data.ts
@@ -149,7 +149,7 @@ export function updateMeanings(word: WordData) {
 			return word
 		})
 		.catch((err) => {
-			console.error(`${word}: ${err}`)
+			console.error(`${word.word}: ${err}`)
 			return word
 		})
 }

src/lib/table-elements/POS.svelte
@@ -8,7 +8,7 @@
 	}
 </script>
 
-<div class="flex items-center justify-center font-mono">
+<div class="flex items-center justify-center font-mono max-md:hidden">
 	{#if word.fetched}
 		<span class="pos px-2 rounded font-bold italic text-primary-content bg-primary">{partOfSpeech.slice(0, 4)}</span>
 	{/if}

commit 093ff0fb1fca9a7971b92e8c099f1f07891e6b4d

add refresh icon and fix bug introduced in f7e406eb

src/lib/BulkImport.svelte
@@ -10,6 +10,7 @@
 		Tooltip,
 	} from "svelte-ux"
 	import listAddIcon from "./icons/list-add.svg?raw"
+	import refreshIcon from "./icons/refresh-cw-alt.svg?raw"
 	import { data, isDark, customSeparator, sepIndex } from "./store"
 	import { updateMeanings, WordData } from "./data"
 	import pThrottle from "p-throttle"
@@ -23,13 +24,14 @@
 	// let separator: string = " "
 	let parsedData: WordData[] = []
 	let loadingDone: boolean[] = []
+	let loading: boolean = false
 	let errorText: string = ""
 	let parsingState: keyof typeof errorStateColors = "info"
 
 	const definedSeps = [
 		{label: ",", raw: ","},
-		{label: String.raw`\n`, raw: String.raw`\n`},
-		{label: String.raw`\r\n`, raw: String.raw`\r\n`},
+		{label: String.raw`\n`, raw: `\n`},
+		{label: String.raw`\r\n`, raw: `\r\n`},
 		{label: "(space)", raw: " "},
 		{label: `Custom (${$customSeparator})`, raw: ""},
 	]
@@ -64,6 +66,12 @@
 				loadingDone[i] = true
 			})()
 		})
+		let timer = setInterval(() => {
+			if (loadingDone.every((v) => v)) {
+				loading = false
+				clearInterval(timer)
+			}
+		}, 500)
 		// (async () => {await Promise.all(promises)})();
 	}
 	function addParsed() {
@@ -74,18 +82,25 @@
 	}
 
 	function formatList(text: string): string[] {
+		loading = true
+		let timer = setTimeout(() => {
+			loading = false
+		}, 100);
 		let sep = definedSeps[$sepIndex].raw
 		if (!text || !sep) return []
-		let list = text.split(sep)
+		let list = text.split(`${sep}`)
 		console.log({ sep: sep, t: text })
 		console.log(list)
 
 		if (list.length == 1) {
-			errorText = String.raw`Separating with ${$sepIndex} only gave 1 length array`
+			errorText = String.raw`Separating with ${definedSeps[$sepIndex].label} only gave 1 length array`
 			parsingState = "error"
 			return list
 		}
+		clearTimeout(timer)
+		loading = true
 		parsingState = "success"
+		errorText = `Got ${list.length} words separated by ${definedSeps[$sepIndex].label}!`
 		return list
 	}
 	// $: console.log(titleNode?.parentElement!.clientHeight)
@@ -100,9 +115,10 @@
 		let shared: string = ""
 		if (params.has("sharedWords")) shared = params.get("sharedWords")!
 
+		open = true
 		inputText = shared
 		$sepIndex = 3 // space
-		open = true
+		parseMultiple(formatList(inputText))
 	}
 	$: textareaHeight = calcH(
 		innerHeight,
@@ -180,14 +196,25 @@
 
 		<div slot="actions" bind:this={actionsNode} class="w-full px-4 gap-2 flex justify-end items-center">
 			<div class="font-mono text-center content-center px-2 mr-auto
-			h-full rounded whitespace-pre {errorStateColors[parsingState]}">
+			h-full rounded whitespace-break-spaces {errorStateColors[parsingState]}">
 				{">"}
 				{#if errorText}
 					{errorText}
 				{:else}
-					Errors will be shown here.
+						Errors will be shown here.
 				{/if}
 			</div>
+			<Button
+				variant="default"
+				color="primary"
+				icon={refreshIcon}
+				{loading}
+				disabled={loading}
+				on:click={(e) => {
+					e.stopPropagation()
+					parseMultiple(formatList(inputText))
+				}}
+			/>
 			<Button
 				variant="fill"
 				color="success"

src/lib/icons/refresh-cw-alt.svg
@@ -1,0 +1,4 @@
+<?xml version="1.0" encoding="utf-8"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+<svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M21 12C21 16.9706 16.9706 21 12 21C9.69494 21 7.59227 20.1334 6 18.7083L3 16M3 12C3 7.02944 7.02944 3 12 3C14.3051 3 16.4077 3.86656 18 5.29168L21 8M3 21V16M3 16H8M21 3V8M21 8H16" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>

src/lib/table-elements/Word.svelte
@@ -10,9 +10,11 @@
 
 <div class="text-center font-bold text-accent-900 dark:text-accent-50">
 	<div>{word.word}</div>
+	{#if word.fetched}
 	<span
 		class="pos px-2 rounded font-mono font-bold italic text-primary-content bg-primary md:hidden"
 	>
 		{partOfSpeech.slice(0, 4)}
 	</span>
+	{/if}
 </div>